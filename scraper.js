5f5bc1cccbd438f9d0353da9c87898330ef0e794
arifinblog <dianarifin1995@gmail.com>
2025-02-12 12:53:35 +0700

    up

--- scraper.js
+++ scraper.js
@@ -197,7 +197,7 @@ app.get('/', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server berjalan di port ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 }); 
